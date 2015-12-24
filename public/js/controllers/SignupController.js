3cb0408909f5f4b122d282c5a02c90a7ab45b951
ni-kun <nicola.ferracin@gmail.com>
2015-12-24 12:00:04 +0100

    fixed issue #1

--- public/js/controllers/SignupController.js
+++ public/js/controllers/SignupController.js
@@ -12,6 +12,7 @@ app.controller('SignupController', ['$scope', '$http', '$window', function($scop
 
 
   $scope.signup = function(account) {
+    resetErrors();
     $scope.submitted = true;
 
     if (account.password != account.repeatPassword) {
@@ -32,8 +33,16 @@ app.controller('SignupController', ['$scope', '$http', '$window', function($scop
     .error(function (data) {
       console.log('Error: ' + data);
       $scope.showErrorAlert = true;
+      $scope.submitted = false;
     });
   };
+
+
+  var resetErrors = function() {
+    $scope.submitted = false;
+    $scope.showPasswordAlert = false;
+    $scope.showErrorAlert = false;
+  }
 
 
 }]);
